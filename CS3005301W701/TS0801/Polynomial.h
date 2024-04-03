cd75a08268fe96aeca1cab2392d8f1938c49717f
waig548 <waig548@hotmail.com>
2024-04-04 00:43:43 +0800

    Week7

---
+++ CS3005301W701/TS0801/Polynomial.h
@@ -0,0 +1,99 @@
+#include <algorithm>
+#include <vector>
+
+class Polynomial {
+    private:
+    std::vector<double> coefs;
+
+    Polynomial(std::vector<double> coefs): coefs(coefs) {
+    }
+
+    public:
+    Polynomial() {
+    }
+    Polynomial(double* param, int size) {
+        for (int i = 0; i < size; i++)
+            coefs.push_back(param[i]);
+    }
+    Polynomial(const Polynomial& copy) {
+        for (auto e: copy.coefs)
+            coefs.push_back(e);
+    }
+    ~Polynomial() {
+    }
+
+    Polynomial& operator=(const Polynomial& copy) {
+        coefs = std::vector<double>();
+        for (auto e: copy.coefs)
+            coefs.push_back(e);
+        return *this;
+    }
+
+    double& operator[](size_t index) {
+        if (index >= coefs.size())
+            coefs.resize(index + 1, 0);
+        return coefs[index];
+    }
+
+    int mySize() {
+        if (coefs.empty())
+            return 0;
+        return coefs.rend() - std::find_if_not(
+                 coefs.rbegin(), coefs.rend(),
+                 [](auto it) {
+                     return it == 0;
+                 });
+    }
+
+    friend static double evaluate(const Polynomial& poly, const double& var) {
+        double res = 0;
+        for (int i = 0; i < poly.coefs.size(); i++) {
+            res += poly.coefs[i] * std::pow(var, i);
+        }
+        return res;
+    }
+
+    friend static Polynomial operator+(const Polynomial& a, const Polynomial& b) {
+        std::vector<double> res(a.coefs);
+        res.resize(std::max(a.coefs.size(), b.coefs.size()));
+        for (int i = 0; i < b.coefs.size(); i++)
+            res[i] += b.coefs[i];
+        return Polynomial(res);
+    }
+    friend static Polynomial operator+(const Polynomial& a, const double& b) {
+        return a + Polynomial(std::vector<double>(1, b));
+    }
+    friend static Polynomial operator+(const double& a, const Polynomial& b) {
+        return b + a;
+    }
+
+    friend static Polynomial operator-(const Polynomial& a, const Polynomial& b) {
+        std::vector<double> res(a.coefs);
+        res.resize(std::max(a.coefs.size(), b.coefs.size()));
+        for (int i = 0; i < b.coefs.size(); i++)
+            res[i] -= b.coefs[i];
+        return Polynomial(res);
+    }
+    friend inline static Polynomial operator-(const Polynomial& a, const double& b) {
+        return a - Polynomial(std::vector<double>(1, b));
+    }
+    friend inline static Polynomial operator-(const double& a, const Polynomial& b) {
+        return Polynomial(std::vector<double>(1, a)) - b;
+    }
+
+    friend static Polynomial operator*(const Polynomial& a, const Polynomial& b) {
+        std::vector<double> res;
+        res.resize(a.coefs.size()+b.coefs.size()-1);
+        for (int i = 0; i < b.coefs.size(); i++) {
+            for (int j = 0; j < a.coefs.size();j++)
+                res[i+j] += a.coefs[j]*b.coefs[i];
+        }
+        return Polynomial(res);
+    }
+    friend inline static Polynomial operator*(const Polynomial& a, const double& b) {
+        return a * Polynomial(std::vector<double>(1, b));
+    }
+    friend inline static Polynomial operator*(const double& a, const Polynomial& b) {
+        return b * a;
+    }
+};
---
+++ CS3005301W701/TS0803/main.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+#include <map>
+#include <sstream>
+#include <string>
+#include <typeindex>
+
+class MemoryBlock {
+    private:
+    char*  block;
+    size_t size;
+
+    public:
+    MemoryBlock(size_t size): size(size) {
+        block = (char*)malloc(size * sizeof(char));
+        if (block)
+            memset(block, 0, size * sizeof(char));
+    }
+    ~MemoryBlock() {
+        delete[] block;
+    }
+
+    void set(int pos, const std::string& type, const std::string& value) {
+        if (pos < 0)
+            throw std::exception();
+        if (type == "String") {
+            auto cur = value.begin();
+            while (pos < size && cur != value.end()) {
+                block[pos] = *cur;
+                cur++;
+                pos++;
+            }
+            if (pos == size)
+                throw std::exception();
+        } else {
+            size_t   typeSize     = typeSizes.at(type);
+            size_t raw          = std::stoull(value);
+            size_t amountToCopy = std::min(size - pos, typeSize);
+            memcpy_s(block + pos, amountToCopy, &raw, amountToCopy);
+            if (pos + typeSize > size)
+                throw std::exception();
+        }
+       
+    }
+
+    std::string get(int pos, const std::string& type) const {
+        if (pos < 0)
+            throw std::invalid_argument("");
+        if (type == "String") {
+            if (pos + typeSizes.at("char") > size)
+                throw std::exception();
+            std::string res;
+            while (pos < size && block[pos]) {
+                res.push_back(block[pos]);
+                pos++;
+            }
+            return res;
+        } else {
+            size_t typeSize = typeSizes.at(type);
+            if (pos + typeSize > size)
+                throw std::exception();
+            unsigned int raw = 0;
+            memcpy_s(&raw, typeSize, block + pos, typeSize);
+            return std::to_string(raw);
+        }
+    }
+
+    static const std::map<std::string, size_t> typeSizes;
+};
+
+const std::map<std::string, size_t> MemoryBlock::typeSizes{{"char", 1}, {"short", 2}, {"int", 4}, {"String", 0}};
+
+int main() {
+    int N, T;
+    std::cin >> N;
+    MemoryBlock block(N);
+    std::cin >> T;
+    while (T--) {
+        std::string op, args, type, value, tmp;
+        int         pos;
+        std::cin >> op;
+        std::getline(std::cin, args);
+
+        std::stringstream prompt(args);
+        prompt >> pos >> type >> value;
+        std::getline(prompt, tmp);
+        value += tmp;
+        if (std::cin.bad() || prompt.bad() || MemoryBlock::typeSizes.find(type) == MemoryBlock::typeSizes.end())
+            continue;
+
+        try {
+            if (op == "Set") {
+                block.set(pos, type, value);
+            } else if (op == "Get") {
+                std::cout << block.get(pos, type) << std::endl;
+            }
+        } catch (std::exception e) {
+            std::cout << "Violation Access." << std::endl;
+        }
+    }
+}
